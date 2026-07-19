a1e6c7f6076cb189035bfea36bc7ac41c07afb31
blexye <marcelobarrios797@gmail.com>
2026-07-18 23:34:20 -0300

    configuración de Dockerfile, compose-docker y migraciones dentro del contenedor

--- BootcampAPI/Program.cs
+++ BootcampAPI/Program.cs
@@ -2,10 +2,12 @@ using BootcampAPI.Application.Common.Behaviors;
 using BootcampAPI.Endpoints;
 using BootcampAPI.Features.Accounts.Commands.CreateAccount;
 using BootcampAPI.Infrastructure;
-using Scalar.AspNetCore;
+using BootcampAPI.Infrastructure.Persistance;
 using BootcampAPI.Middleware;
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 using MediatR;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,18 +35,21 @@ var app = builder.Build();
 
 app.UseValidationExceptionHandling();
 
-if (app.Environment.IsDevelopment())
-{
-	app.MapOpenApi();
-	app.MapScalarApiReference();
+app.MapOpenApi();
+app.MapScalarApiReference();
 
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.MapAccountEndpoints();
+
+using (var scope = app.Services.CreateScope())
+{
+	var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+	db.Database.Migrate();
+}
 
 app.Run();
--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
 FROM mcr.microsoft.com/dotnet/sdk:10.0 AS build
 WORKDIR /src
 
-COPY . .
+COPY . /src
+RUN dotnet restore "BootcampAPI/BootcampAPI.Api.csproj"
 
-RUN dotnet restore "BootcampAPI.csproj"
-
-RUN dotnet publish "BootcampAPI.csproj" \
+RUN dotnet publish "BootcampAPI/BootcampAPI.Api.csproj" \
     -c Release -o /app/publish --no-restore
 
 FROM mcr.microsoft.com/dotnet/aspnet:10.0 AS final
@@ -15,4 +14,4 @@ COPY --from=build /app/publish .
 ENV ASPNETCORE_HTTP_PORTS=8080
 EXPOSE 8080
 
-ENTRYPOINT ["dotnet", "BootcampAPI.dll"]
+ENTRYPOINT ["dotnet", "BootcampAPI.Api.dll"]
